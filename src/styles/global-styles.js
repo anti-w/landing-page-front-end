546c7b44b4566b0e8f57dfd843bd117283a9803c
Wendel Demétrio <hey.wendel95@gmail.com>
2022-02-21 10:42:11 -0300

    creating component Heading pt1

--- src/styles/global-styles.js
+++ src/styles/global-styles.js
@@ -7,6 +7,7 @@ export const GlobalStyles = createGlobalStyle`
 * {
   margin: 0;
   padding: 0;
+  box-sizing: border-box;
 }
 
 html {
@@ -19,7 +20,7 @@ body {
 
 }
 
-h1 {
+h1, h2, h3, h4, h5, h6 {
   font-family: ${({ theme }) =>
     theme.fonts.family.secondary};
 }
