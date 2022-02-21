bf5c1910f4284fc64b45b88554c1cab9c01e71a8
Wendel Demétrio <hey.wendel95@gmail.com>
2022-02-21 09:36:35 -0300

    Heading component with styled-component on sb

--- src/templates/Home/index.jsx
+++ src/templates/Home/index.jsx
@@ -1,10 +1,11 @@
+import { Heading } from '../../components/Heading';
 import * as Styled from './styles';
 
 function Home() {
   return (
     <div className="Home">
       <Styled.Wrapper>
-        <h1>Hello</h1>
+        <Heading>Olá!</Heading>
       </Styled.Wrapper>
     </div>
   );
