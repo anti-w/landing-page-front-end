500d156636d75a50210501116e70eab2ba5693cb
Wendel Demétrio <hey.wendel95@gmail.com>
2022-02-21 07:46:34 -0300

    i && config ThemeProvider from styled-components

--- src/index.jsx
+++ src/index.jsx
@@ -1,10 +1,17 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import Home from "./templates/Home";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { ThemeProvider } from 'styled-components';
+
+import { GlobalStyles } from './styles/global-styles';
+import { theme } from './styles/theme';
+import Home from './templates/Home';
 
 ReactDOM.render(
   <React.StrictMode>
-    <Home />
+    <ThemeProvider theme={theme}>
+      <GlobalStyles />
+      <Home />
+    </ThemeProvider>
   </React.StrictMode>,
-  document.getElementById("root")
+  document.getElementById('root'),
 );
