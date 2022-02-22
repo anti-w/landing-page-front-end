7dd12f8734d79f09a5cfa9976abac02664b7a56a
Wendel Demétrio <hey.wendel95@gmail.com>
2022-02-22 10:18:38 -0300

    component MenuLink with tests

---
+++ src/components/MenuLink/MenuLink.test.jsx
@@ -0,0 +1,34 @@
+import { screen } from '@testing-library/react';
+import { MenuLink } from '.';
+import { renderTheme } from '../../styles/render-theme';
+
+describe('<MenuLink />', () => {
+  it('should render a link', () => {
+    renderTheme(
+      <MenuLink link="http://localhost">Children</MenuLink>,
+    );
+    expect(
+      screen.getByRole('link', { name: 'Children' }),
+    ).toHaveAttribute('target', '_self');
+  });
+
+  it('should open link in new tab', () => {
+    renderTheme(
+      <MenuLink newTab link="http://localhost">
+        Children
+      </MenuLink>,
+    );
+    expect(
+      screen.getByRole('link', { name: 'Children' }),
+    ).toHaveAttribute('target', '_blank');
+  });
+
+  it('should match snapshot', () => {
+    renderTheme(
+      <MenuLink link="http://localhost">Children</MenuLink>,
+    );
+    expect(
+      screen.getByRole('link', { name: 'Children' }),
+    ).toMatchSnapshot();
+  });
+});
---
+++ src/components/MenuLink/index.jsx
@@ -0,0 +1,21 @@
+import P from 'prop-types';
+import * as Styled from './styles';
+
+export const MenuLink = ({
+  children,
+  link,
+  newTab = false,
+}) => {
+  const target = newTab ? '_blank' : '_self';
+  return (
+    <Styled.Container target={target} href={link}>
+      {children}
+    </Styled.Container>
+  );
+};
+
+MenuLink.propTypes = {
+  children: P.node.isRequired,
+  link: P.string.isRequired,
+  newTab: P.bool,
+};
