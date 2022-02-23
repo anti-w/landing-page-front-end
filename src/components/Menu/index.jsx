430f6c3bb63c492844e9af15d115d38f6865f72a
Wendel Demétrio <hey.wendel95@gmail.com>
2022-02-23 10:59:10 -0300

    component Menu pt2

--- src/components/Menu/index.jsx
+++ src/components/Menu/index.jsx
@@ -1,4 +1,8 @@
 import P from 'prop-types';
+import { useState } from 'react';
+
+import { Menu as MenuIcon } from '@styled-icons/material-outlined/Menu';
+import { Close as CloseIcon } from '@styled-icons/material-outlined/Close';
 
 import * as Styled from './styles';
 
@@ -7,15 +11,33 @@ import { LogoLink } from '../LogoLink';
 import { NavLink } from '../NavLink';
 
 export const Menu = ({ links = [], logoData }) => {
+  const [visible, setVisible] = useState(false);
+
   return (
-    <Styled.Container>
-      <SectionContainer>
-        <Styled.MenuContainer>
-          <LogoLink {...logoData} />
-          <NavLink links={links} />
-        </Styled.MenuContainer>
-      </SectionContainer>
-    </Styled.Container>
+    <>
+      <Styled.Button
+        aria-label="Open/Close Menu"
+        visible={visible}
+        onClick={() => setVisible(true)}
+      >
+        {visible ? (
+          <CloseIcon aria-label="Close menu" />
+        ) : (
+          <MenuIcon aria-label="Open menu" />
+        )}
+      </Styled.Button>
+      <Styled.Container
+        onClick={() => setVisible(false)}
+        visible={visible}
+      >
+        <SectionContainer>
+          <Styled.MenuContainer>
+            <LogoLink {...logoData} />
+            <NavLink links={links} />
+          </Styled.MenuContainer>
+        </SectionContainer>
+      </Styled.Container>
+    </>
   );
 };
 
