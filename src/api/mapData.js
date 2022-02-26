adf1a062b284a8b0a3663d02dd3046df65b7858c
Wendel Demétrio <hey.wendel95@gmail.com>
2022-02-26 08:29:38 -0300

    mapping data with tests

--- src/api/mapData.js
+++ src/api/mapData.js
@@ -1,8 +1,19 @@
-export const mapData = () => {
-  return {
-    footerHtml: '',
-    slug: '',
-    title: '',
-    sections: [],
-  };
+export const mapData = (pagesData = [{}]) => {
+  return pagesData.map((data) => {
+    const {
+      footer_text: footerHtml = '',
+      slug = '',
+      title = '',
+      sections = [],
+      menu = {},
+    } = data;
+
+    return {
+      footerHtml,
+      slug,
+      title,
+      sections,
+      menu,
+    };
+  });
 };
