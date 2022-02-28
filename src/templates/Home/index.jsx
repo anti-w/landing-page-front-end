64ac949927aba2b07860c0420f857b311eb2e01f
Wendel Demétrio <hey.wendel95@gmail.com>
2022-02-28 08:19:11 -0300

    fetching data  on front-end

--- src/templates/Home/index.jsx
+++ src/templates/Home/index.jsx
@@ -1,7 +1,29 @@
+import { useEffect, useState } from 'react';
 import { Base } from '../Base';
 import { mockBase } from '../Base/mock';
 
+import { mapData } from '../../api/map-data';
+
 function Home() {
+  const [data, setData] = useState([]);
+  useEffect(() => {
+    const load = async () => {
+      const data = await fetch(
+        'http://localhost:1337/pages/?slug=droid-landing-page',
+      );
+      const json = await data.json();
+      const filteredData = mapData(json);
+      setData(filteredData[0]);
+    };
+
+    load();
+  }, []);
+
+  if (data === undefined)
+    return <h1>Página não encontrada</h1>;
+
+  if (data && !data.slug) return <h1>Carregando...</h1>;
+
   return <Base {...mockBase} />;
 }
 
